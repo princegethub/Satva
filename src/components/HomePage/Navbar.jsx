2d60297cb1c59fb94782acc8d9e858a2c3c8775f
Prince Mishra <pm618209@gmail.com>
2025-08-28 00:23:07 +0530

    fix: update Sidebar import path in Navbar component

--- src/components/HomePage/Navbar.jsx
+++ src/components/HomePage/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Logo from "@assets/logo.png";
 import { Link, useLocation } from "react-router-dom";
 import { Menu, Facebook, Instagram, Twitter, House } from "lucide-react";
-import Sidebar from "@/components/Sidebar.jsx";
+import Sidebar from "@/components/Sidebar";
 
 // ✅ Navbar ke links ka data
 const navLinks = [
